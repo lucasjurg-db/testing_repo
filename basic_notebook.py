7f774283c4dccb0cc75d5ac0308e6ee020fe266b
Lucas Jurgensen <lucas.jurgensen@databricks.com>
2024-09-16 13:59:29 -0700

    Update basic_notebook.py

--- basic_notebook.py
+++ basic_notebook.py
@@ -53,7 +53,7 @@ for _ in range(10):
 # with open("filex00.py", "w") as f:
 #   f.write("print('Hello World Again')")
 
-# Another comment
+# Another comment again
 import os
 
 # file_path = '/Workspace/lucas'
